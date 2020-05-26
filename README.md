07bf016bd2ca526a42799fbd6cedd248ae2899e5
JT Archie <jarchie@pivotal.io>
2020-05-26 10:45:34 -0600

    switch to sinatra server

--- README.md
+++ README.md
@@ -5,4 +5,12 @@ bundle
 ragel -R -e -F1 lib/parse.rl
 rubocop -a
 rspec
+ruby build.rb # generates a list of only US cities at the moment
+```
+
+# Run
+
+```sh
+ruby generate.rb
+# load in browser
 ```
--- generate.rb
+++ generate.rb
@@ -2,44 +2,63 @@
 
 require 'json'
 require 'ostruct'
-require 'erb'
+require 'sinatra'
 require_relative 'lib/parse'
 
 include ERB::Util
 
-cities = JSON.parse(File.read('us.json')).collect { |c| OpenStruct.new(c) }
-
-queries = [
-  'q:"jeep truck" include_nearby has-image bundle-duplicates auto-year:<1985',
-  'q:"jeep j10" include_nearby has-image bundle-duplicates auto-year:<1985',
-  'q:"jeep j20" include_nearby has-image bundle-duplicates auto-year:<1985'
-].map { |q| Parser.parse(q) }
-
-top_cities = cities
+cities     = JSON.parse(File.read('us.json')).collect { |c| OpenStruct.new(c) }
+TOP_CITIES = cities
              .sort_by { |c| c.nearby_cities.length }
              .reverse
              .uniq(&:region_name)
              .take(50)
              .sort_by(&:region_name)
 
-template = <<~ERB
-  <html>
-      <head>
-          <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
-          <meta name="viewport" content="width=device-width, initial-scale=1">
-      </head>
-      <body>
+get '/' do
+  @parsed_query = Parser.parse(@query) if @query = params[:query]
+
+  @example_query = 'q:"jeep truck" include_nearby has-image bundle-duplicates auto-year:<1985'
+  @top_cities    = TOP_CITIES
+
+  erb :queries
+end
+
+__END__
+
+@@ queries
+<html>
+  <head>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/turbolinks/5.2.0/turbolinks.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/mini.css/3.0.1/mini-default.min.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <style>
+        form input {
+            width: 100%;
+        }
+      </style>
+  </head>
+  <body>
       <div class="container">
-          <% top_cities.each do |city| %>
-              <h1><%= city.country_name %> / <%= city.region_name %> / <%= city.name %></h1>
-              <ol>
-                  <% queries.each do |query| %>
-                      <li><a target="_blank" href="<%= query.url(city: city) %>"><%= h query.to_s %></a></li>
+          <div class="row">
+              <div class="col-sm-12 col-md-12 col-lg-12">
+                  <form action="/">
+                      <input type="text" value="<%= h @query %>" placeholder="q:'classic guitar' year:<1995 include-pics" name="query">
+                      <p>Example: <a href="/?query=<%= u 'q:"jeep truck" include_nearby has-image bundle-duplicates auto-year:<1985' %>">q:"jeep truck" include_nearby has-image bundle-duplicates auto-year:<1985</a></p>
+                  </form>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-sm-12 col-md-12 col-lg-12">
+              <% if @query %>
+                  <ul>
+                  <% @top_cities.each do |city| %>
+                    <li><a target="_blank" href="<%= @parsed_query.url(city: city) %>"><%= city.country_name %> / <%= city.region_name %> / <%= city.name %></a></li>
                   <% end %>
-              </ol>
-          <% end %>
+                  </ul>
+              <% end %>
+              </div>
+          </div>
       </div>
-      </body>
-  </html>
-ERB
-puts ERB.new(template).result(binding)
+  </body>
+</html>
