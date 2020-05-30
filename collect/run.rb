778ad3c95c4fab8d5ec153a799b9a7ff778f5ed3
Kira Boyle <kboyle@vmware.com>
2020-05-29 22:02:28 -0600

    all cities
    Signed-off-by: JT Archie <jtarchie@gmail.com>

--- collect/run.rb
+++ collect/run.rb
@@ -4,15 +4,17 @@ require_relative 'lib/craigslist'
 require 'json'
 
 cl = Craigslist.new
-us_cities = cl.cities.by_country(name: 'US')
-payload = us_cities.map do |city|
-  {
+payload = cl.cities.map do |city|
+  print "city: #{city.name} - "
+  v = {
     name: city.name,
     region_name: city.region_name,
     country_name: city.country_name,
     nearby_cities: city.nearby_cities,
     url: city.url
   }
+  puts 'done'
+  v
 end
 
 File.write(File.join(DATA_DIR, 'processed.json'), JSON.pretty_generate(payload))
